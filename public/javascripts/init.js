242a52fde2b11563079ad83993a8e8f15a3a902c
Yuvraj <kumaryuvraj118@gmail.com>
2016-08-04 03:03:37 +0530

    Added files

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,15 +1,4 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-    	// console.log($(window).scrollTop()+" scroll height ");
-        if($(window).scrollTop() > 550){
-        	
-            $("nav").removeClass("header-trans").addClass("green-nav").addClass('customNav').addClass('z-depth-2').removeClass('z-depth-0');
-        }else {
-    		$("nav").removeClass("green-nav").addClass("header-trans").removeClass('z-depth-2').addClass('z-depth-0');
-   		}
 
-    });
-});
 
 $(document).ready(function(){
 	$('.slider').slider({full_width: true,indicators: false});
@@ -25,11 +14,13 @@ $(".button-collapse").sideNav();
 
 var $form = $('#mail-form');
 $form.on('submit', function() { 
-	$(this).find("button[type='submit']").prop('disabled','disabled'); 
+	$(this).find("button[type='submit']").prop('disabled','disabled');
+	$(".submit").text("SENDING"); 
 	 $("#loader").show();         
 	$.post($form.attr('action'), $form.serialize())
 		.done(function(data) {
 			$("#submit-button").prop('disabled',false); 
+			$(".submit").text("SEND")
 	 		$("#loader").hide();
 			$(".message").html("<br><br>We've got your message.We'll get back to you soon !<br><br>");
 		})
