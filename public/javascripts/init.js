dcaea13f5f8fe835d2a5e1399090ff3bbcb72e88
Yuvraj <kumaryuvraj118@gmail.com>
2016-08-03 15:59:55 +0530

    landing page created

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -21,3 +21,29 @@ $(document).ready(function(){
 });
 
 $(".button-collapse").sideNav();
+
+
+var $form = $('#mail-form');
+$form.on('submit', function() { 
+	$(this).find("button[type='submit']").prop('disabled','disabled'); 
+	 $("#loader").show();         
+	$.post($form.attr('action'), $form.serialize())
+		.done(function(data) {
+			$("#submit-button").prop('disabled',false); 
+	 		$("#loader").hide();
+			$(".message").html("<br><br>We've got your message.We'll get back to you soon !<br><br>");
+		})
+		.fail(function(jqXHR, textStatus, errorThrown) {
+			$('#mail-form').find('input:text').val(''); 
+			$('#mail-form').find('input:email').val('');
+			$('textarea').val('');
+			$("submit-button").prop('disabled',false); 
+	 		$("#loader").hide();
+			$(".message").html("<br><br> Something went wrong, please try again.<br><br>");
+		});
+	return false;
+}); 
+
+$(document).ready(function(){
+	$('.modal-trigger').leanModal();
+});
